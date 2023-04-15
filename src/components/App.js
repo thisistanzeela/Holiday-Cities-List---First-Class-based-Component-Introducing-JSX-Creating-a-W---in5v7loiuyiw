57327540c887307af0f8bbc272b74e49282758fe
thisistanzeela <86124816+thisistanzeela@users.noreply.github.com>
2023-04-15 22:35:14 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -38,9 +38,18 @@ class App extends Component {
   }
 
   render() {
+    const indianCities = this.cityList.filter(
+      (city) => city.country === "India"
+    );
     return (
       <div id="main">
         {/* Do not remove the main div */}
+         <h1>Indian Cities</h1>
+        <ol>
+          {indianCities.map((city, index) => (
+            <li key={`city${index + 1}`}>{city.name}</li>
+          ))}
+        </ol>
       </div>
     )
   }
