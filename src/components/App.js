5ad21e406267039087e2f203fab8778138b5c328
thisistanzeela <86124816+thisistanzeela@users.noreply.github.com>
2023-04-18 21:49:40 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -33,7 +33,7 @@ class App extends Component {
     { name: 'Amritsar', country: 'India' },
     { name: 'Mussoorie', country: 'India' },
     { name: 'Mount Abu', country: 'India' },
-    { name: 'Tirupati', country: 'India' },
+    { name: 'Tirupatiii', country: 'India' },
        ]
   }
 render() {
